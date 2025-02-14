73df65b9e5bea4b372fc495fb77241192ee9309a
godinezsteven1 <steven.godin.42@gmail.com>
2025-02-14 15:28:18 -0500

    updated json files, fixed picture path, added git and LinkedIn Link, added new nav section

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,154 +1,145 @@
 export default function Layout({ children }) {
     return (
-        <div className="min-h-screen bg-transparent text-gray-200 px-6"> {/* do not change transperance  */}
+        <div
+            className="min-h-screen bg-transparent text-gray-200 px-6"> {/* do not change transperance  */}
             <div className="mb-8">
                 {children}
             </div>
-            <div className="max-w-xl mx-auto">
-                <div className="relative rounded-lg bg-slate-900 p-2">
-                    <div className="relative flex text-center">
-                        <div className="flex pl-3.5 pt-3">
-                            <svg viewBox="0 0 24 24" fill="currentColor" className="-ml-0.5 mr-1.5 h-3 w-3 text-red-500/20">
-                                <circle r="12" cy="12" cx="12"></circle>
-                            </svg>
-                            <svg viewBox="0 0 24 24" fill="currentColor" className="-ml-0.75 mr-1.5 h-3 w-3 text-yellow-500/20">
-                                <circle r="12" cy="12" cx="12"></circle>
-                            </svg>
-                            <svg viewBox="0 0 24 24" fill="currentColor" className="-ml-0.75 mr-1.5 h-3 w-3 text-green-500/20">
-                                <circle r="12" cy="12" cx="12"></circle>
-                            </svg>
+
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+                    <div className="max-w-xl bg-slate-900 rounded-lg p-4 shadow-lg">
+                        <div
+                            className="relative flex justify-between items-center px-4 py-2 bg-slate-800 rounded-t-lg">
+                            <div className="flex space-x-2">
+                                <span className="w-3 h-3 bg-red-500 rounded-full"></span>
+                                <span className="w-3 h-3 bg-yellow-500 rounded-full"></span>
+                                <span className="w-3 h-3 bg-green-500 rounded-full"></span>
+                            </div>
+                            <span className="text-xs text-slate-500">profile.json</span>
                         </div>
-                        <span className="absolute inset-x-0 top-2 text-xs text-slate-500">profile.json</span>
+
+                        <div className="mt-4 px-5 pb-6 font-mono text-sm text-gray-300">
+                            <p className="text-slate-400">{`{`}</p>
+                            <p className="text-pink-400">
+                                "info": <span className="text-white">{`{`}</span>
+                            </p>
+                            <p className="ml-8"><span className="text-pink-400">"email":</span>
+                                <span className="text-blue-300"> "steven.godin.42@gmail.com"</span>,
+                            </p>
+                            <p className="ml-8"><span className="text-pink-400">"university":</span>
+                                <span className="text-blue-300"> "Northeastern University"</span>,
+                            </p>
+
+                            <p className="ml-8"><span className="text-pink-400">"Languages":</span>
+                                <span className="text-slate-400"> [</span></p>
+                            <p className="ml-12"><span className="text-blue-300">"Java"</span>,</p>
+                            <p className="ml-12"><span className="text-blue-300">"Python"</span>,
+                            </p>
+                            <p className="ml-12"><span className="text-blue-300">"JavaScript"</span>,
+                            </p>
+                            <p className="ml-12"><span className="text-blue-300">"HTML"</span>,</p>
+                            <p className="ml-12"><span className="text-blue-300">"Tailwind"</span>
+                            </p>
+                            <p className="ml-8 text-slate-400">],</p>
+
+                            <p className="ml-8"><span
+                                className="text-pink-400">"Frameworks-and-tech":</span> <span
+                                className="text-slate-400"> [</span></p>
+                            <p className="ml-12"><span className="text-blue-300">"Next.js"</span>,
+                            </p>
+                            <p className="ml-12"><span className="text-blue-300">"Flask"</span>,</p>
+                            <p className="ml-12"><span className="text-blue-300">"Git"</span>,</p>
+                            <p className="ml-12"><span className="text-blue-300">"PostgreSQL"</span>,
+                            </p>
+                            <p className="ml-12"><span className="text-blue-300">"Vercel"</span></p>
+                            <p className="ml-8 text-slate-400">],</p>
+                            {/*here */}
+
+                            <p className="ml-8"><span
+                                className="text-pink-400">"AI":</span> <span
+                                className="text-slate-400"> [</span></p>
+                            <p className="ml-12"><span className="text-blue-300">"numpy"</span>,
+                            </p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"OpenAI gymnasium"</span></p>
+                            <p className="ml-8 text-slate-400">],</p>
+
+                            <p className="ml-8"><span
+                                className="text-pink-400">"Cybersecurity":</span> <span
+                                className="text-slate-400"> [</span></p>
+                            <p className="ml-12"><span className="text-blue-300">"Linux"</span>,</p>
+                            <p className="ml-12"><span className="text-blue-300">"ParrotOS"</span>,</p>
+                            <p className="ml-12"><span className="text-blue-300">"Malware"</span></p>
+                            <p className="ml-8 text-slate-400">]</p>
+                            <p className="text-slate-400">{`}`}</p>
+                        </div>
                     </div>
-                    <div className="mt-5 space-y-1.5 px-5 pb-10 text-left font-mono text-xs">
-                        <p className="text-slate-400">{`{`}</p>
-                        <p className="text-pink-400">
-                            "info": <span className="text-white">{`{`}</span>
-                        </p>
-                        <p className="ml-8">
-                            <span className="text-pink-400">"email":</span>
-                            <span
-                                className="text-blue-300"> "steven.godin.42@gmail.com"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-8">
-                            <span className="text-pink-400">"university":</span>
-                            <span className="text-blue-300"> "Northeastern University"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
 
+                    <div className="max-w-xl bg-slate-900 rounded-lg p-4 shadow-lg">
+                        <div
+                            className="relative flex justify-between items-center px-4 py-2 bg-slate-800 rounded-t-lg">
+                            <div className="flex space-x-2">
+                                <span className="w-3 h-3 bg-red-500 rounded-full"></span>
+                                <span className="w-3 h-3 bg-yellow-500 rounded-full"></span>
+                                <span className="w-3 h-3 bg-green-500 rounded-full"></span>
+                            </div>
+                            <span className="text-xs text-slate-500">profile.json</span>
+                        </div>
 
-                        <p className="ml-8">
-                            <span className="text-pink-400">"Languages":</span>
-                            <span className="text-slate-400"> [</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Java"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Python"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"JavaScript"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"HTML"</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Tailwind"</span>
-                        </p>
-                        <p className="ml-8 text-slate-400">],</p>
+                        <div className="mt-4 px-5 pb-6 font-mono text-sm text-gray-300">
+                            <p className="text-slate-400">{`{`}</p>
+                            <p className="text-pink-400">
+                                "bio": <span className="text-white">{`{`}</span>
+                            </p>
+                            <p className="ml-8"><span className="text-pink-400">"hobbies":</span>
+                                <span className="text-slate-400"> [</span></p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Motorcycles"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Powerlifting"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Anime"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Fishing"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Arduino"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Electronic Tinkering"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Ethical Hacking"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"3D Design"</span>,</p>
+                            <p className="ml-12"><span
+                                className="text-blue-300">"Algortithmic Reasoning"</span>,</p>
+                            <p className="ml-8 text-slate-400">],</p>
 
+                            <p className="ml-8"><span className="text-pink-400">"Current-Projects":</span>
+                                <span className="text-slate-400"> [</span></p>
+                            <p className="ml-12"><span className="text-blue-300">"AI-SignLanguage to text"</span>,
+                            </p>
+                            <p className="ml-12"><span className="text-blue-300">"InTouchBHl Web + Ticket Management"</span>,
+                            </p>
+                            <p className="ml-8 text-slate-400">]</p>
 
-                        <p className="ml-8">
-                            <span className="text-pink-400">"Frameworks-and-tech":</span>
-                            <span className="text-slate-400"> [</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Next.js"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Flask"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Git"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"PostgreSQL"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Vercel"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-8 text-slate-400">],</p>
+                            <p className="ml-8"><span className="text-pink-400">"Roles":</span>
+                                <span className="text-slate-400"> [</span></p>
+                            <p className="ml-12"><span className="text-blue-300">"Contract Software Engineer"</span>,
+                            </p>
+                            <p className="ml-12"><span className="text-blue-300">"Software Developer"</span>,
+                            </p>
+                            <p className="ml-12"><span className="text-blue-300">"Open Source Contributor"</span>
+                            </p>
+                            <p className="ml-8 text-slate-400">]</p>
 
-
-                        <p className="ml-8">
-                            <span className="text-pink-400">"AI":</span>
-                            <span className="text-slate-400"> [</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"OpenAI Gymnasium"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"numpy"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-8 text-slate-400">],</p>
-
-
-                        <p className="ml-8">
-                            <span className="text-pink-400">"Cybersecurity":</span>
-                            <span className="text-slate-400"> [</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Linux"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"ParrotOS"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Malware"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-8 text-slate-400">],</p>
-
-
-                        <p className="ml-8">
-                            <span className="text-pink-400">"Roles":</span>
-                            <span className="text-slate-400"> [</span>
-                        </p>
-                        <p className="ml-12">
-                            <span
-                                className="text-blue-300">"Contract Software Engineer"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span
-                                className="text-blue-300">"Software Developer"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-                        <p className="ml-12">
-                            <span className="text-blue-300">"Open source contributor"</span><span
-                            className="text-slate-400">,</span>
-                        </p>
-
-                        <p className="ml-8 text-slate-400">]</p>
-                        <p className="ml-4 text-slate-400">{`}`}</p>
-                        <p className="text-slate-400">{`}`}</p>
+                            <p className="text-slate-400">{`}`}</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
+
     )
 }
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,26 +5,64 @@ export default function HomePage() {
 
                 {/* this is left  */}
                 <div className="relative flex justify-center">
-                    <div className="relative w-64 h-64 border-4 border-purple-400 rounded-lg overflow-hidden shadow-lg">
-                        <img src="../../../public/static/about/image1.png" alt="Profile" className="w-full h-full object-cover" />
+                    <div
+                        className="w-64 h-64 sm:w-72 sm:h-72 rounded-lg border-2 border-purple-400 object-cover">
+                        <img src="/static/about/image1.png" alt="Profile"
+                             className="w-full h-full object-cover"/>
                     </div>
                 </div>
 
                 {/* this is right */}
                 <div className="space-y-4">
                     <h1 className="text-5xl font-bold text-purple-400">Steven Godinez</h1>
-                    <p className="text-xl text-gray-400">Software Engineer | AI & Cybersecurity Enthusiast</p>
+                    <p className="text-xl text-gray-400">Software Engineer | AI & Cybersecurity
+                        Enthusiast</p>
                     <p className="text-lg text-gray-300">
                         Hi! I am a current Sophomore at <span className="text-purple-300">Northeastern University </span>
-                        studying <span className="text-purple-300"> Computer Science and Mathematics</span>. I have a passion for
+                        studying <span
+                        className="text-purple-300"> Computer Science and Mathematics</span>. I have
+                        a passion for
                         <span className="text-purple-300"> Artificial Intelligence</span>.
-                        I am simultaneously pursuing my Master's degree in Artificial Intelligence with a specialization in
-                        <span className="text-purple-300"> Machine Learning</span>.
-                    </p>
-                    <p className="text-md text-gray-500">
-                    ahh some quote could go here
+                        I am simultaneously pursuing my Master's degree in Artificial Intelligence
+                        with a specialization in
+                        <span className="text-purple-300"> Machine Learning</span>. All of my code
+                        is
+                        open source!
                     </p>
                 </div>
+
+
+                <div className="flex justify-end space-x-4 mt-4">
+                    {/* GitHub Icon */}
+                    <a href="https://github.com/godinezsteven1" target="_blank"
+                       rel="noopener noreferrer">
+                        <img
+                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg"
+                            height="70"
+                            width="70"
+                            alt="GitHub logo"
+                            className="invert filter brightness-0"
+                        />
+                    </a>
+                    {/* LinkedIn Icon */}
+                    <a href="https://linkedin.com/in/godinezsteven" target="_blank"
+                       rel="noopener noreferrer">
+                        <img
+                            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linkedin/linkedin-original.svg"
+                            height="70"
+                            width="70"
+                            alt="LinkedIn logo"
+                            className="hover:opacity-80 transition"
+                        />
+                    </a>
+                    {/* I wish i had more social media*/}
+                </div>
+
+
+
+
+
+
 
             </div>
         </div>
